2676ec4614570dd973e46cec2c966338419284c7
kostya-nik-a <35659234+kostya-nik-a@users.noreply.github.com>
2018-02-16 13:12:29 +0700

    Update test.php

--- test.php
+++ test.php
@@ -3,20 +3,15 @@ $test_number = ($_GET['test_number']);
 $testDir = "./tests/";
 $tests_list = scandir($testDir);
 $numFiles=count(scandir($testDir))-1;
-
 if ($test_number < 1 || !ctype_digit($test_number) || $test_number > count($tests_list) - 2) {
    echo "Такого теста не существует. Выберите существующий номер теста. <a href='admin.php'> Назад </a>";
    exit();
 }
-
 $test = $tests_list[$test_number+1];
 $contents = file_get_contents(__DIR__ . $testDir.DIRECTORY_SEPARATOR.$test);
 $tests = json_decode($contents, true);
-
 echo "<pre>";
 print_r($tests);
-
-
 ?>
 
 <!DOCTYPE html>
@@ -30,34 +25,18 @@ print_r($tests);
         <form action="" method="POST">
             <div>
                 <?php 
+                $i = 0;
                 foreach ($tests as $tkey => $test) {
                 ?>    
-                <h2><?php echo $test['question'] ?></h2>                               
-                <label>
-                    <input  name="answer1" type="radio" value="<?php echo $test['answers'][0]; ?>"> 
-                    <?php echo $test['answers'][0] ?>
-                </label>
-                <br>
-                <label>
-                    <input  name="answer2" type="radio" value="<?php echo $test['answers'][1]; ?>"> 
-                    <?php echo $test['answers'][1]; ?>
-                </label>
-                <br>
-                <label>
-                    <input  name="answer3" type="radio" value="<?php echo $test['answers'][2]; ?>"> 
-                    <?php echo $test['answers'][2]; ?>
-                </label>
-                <label>
-                    <input  name="answer4" type="radio" value="<?php echo $test['answers'][3]; ?>"> 
-                    <?php echo $test['answers'][3]; ?>
-                </label>
-
-
-                <br>
-                <?php                   
+                <h2><?php echo $test['question']; ?></h2>                               
+                <?php for ($i = 0; $i < count($test['answers']); $i++) { ?>
+                <input name="answer<?=$i+1?>" type="radio" value="<?php echo $test['answers'][0]; ?>"> 
+                <?php echo $test['answers'][$i] ?><br>
+                <?php  
+                      }                 
                 }
                 ?>
-                <label> Введите свое имя и фамилию <input  name="user_name" type="text" value=""></label><br> 
+
                 <button type="submit">Результат</button>   
                                        
             </div>
